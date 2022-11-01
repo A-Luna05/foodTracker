e7f4df6f1f03d25e0fa6faa3d476b965f05ebc13
22joshuao02 <117215717+22joshuao02@users.noreply.github.com>
2022-11-01 15:47:42 -0500

    Update DiaryEntry.java
    Not entirely sure if this is the right way to approach this

--- foodTracker/src/model/DiaryEntry.java
+++ foodTracker/src/model/DiaryEntry.java
@@ -1,17 +1,21 @@
 package model;
 
+import java.util.List;
+import java.util.Date;
+
 public class DiaryEntry {
-  private Meal First;
+	private Meal First;
 	private Meal Second;
 	private Meal Third;
-	public Day Date;
-	private Food snack;
+	public static Day Date;
+	private Meal snacks;
 	public int waterQty;
 	
 	private List<Day> days;
 	
-	private Day getDay(String date) {
-		return null;
+	public static Date getDay(Day date) {
+		java.util.Date Day = new java.util.Date();
+		return Day;
 	}
 	public void update() {
 		
