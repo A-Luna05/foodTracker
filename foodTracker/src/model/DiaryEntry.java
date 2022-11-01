bca68b24692970082cf55046e17638bd9bc6f19e
22joshuao02 <117215717+22joshuao02@users.noreply.github.com>
2022-11-01 15:19:04 -0500

    Update DiaryEntry.java

--- foodTracker/src/model/DiaryEntry.java
+++ foodTracker/src/model/DiaryEntry.java
@@ -1,5 +1,22 @@
 package model;
 
 public class DiaryEntry {
-
+  private Meal First;
+	private Meal Second;
+	private Meal Third;
+	public Date Date;
+	private Food snack;
+	public int waterQty;
+	
+	private List<Day> days;
+	
+	private Day getDay(String date) {
+		return null;
+	}
+	public void update() {
+		
+	}
+	public Meal getMeal(int meal) {
+		
+	}
 }
